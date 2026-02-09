ff369b6c59ff586c635ac4676e05fdfbef65d0f7
santuu2024 <santosh.d.ambhore2001@gmail.com>
2026-02-09 12:55:07 +0530

    Add files via upload
    Task 1 uploaded

---
+++ temp_converter.py
@@ -0,0 +1,27 @@
+# Temperature Conversion Program
+
+# input from user
+temperature = float(input("Enter temperature value: "))
+unit = input("Enter unit (C / F / K): ").upper()
+
+# Conversion logic
+if unit == "C":
+    fahrenheit = (temperature * 9/5) + 32
+    kelvin = temperature + 273.15
+    print(f"Fahrenheit: {fahrenheit:.2f} °F")
+    print(f"Kelvin: {kelvin:.2f} K")
+
+elif unit == "F":
+    celsius = (temperature - 32) * 5/9
+    kelvin = celsius + 273.15
+    print(f"Celsius: {celsius:.2f} °C")
+    print(f"Kelvin: {kelvin:.2f} K")
+
+elif unit == "K":
+    celsius = temperature - 273.15
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"Celsius: {celsius:.2f} °C")
+    print(f"Fahrenheit: {fahrenheit:.2f} °F")
+
+else:
+    print("Invalid unit! Please enter C, F, or K.")
